44c398c9b8ed3e0190a45a7f809f7e6a533ceb83
Bill Su <subiao1215@gmail.com>
2019-09-25 12:25:25 -0600

    Update HellowJava.java

--- basic-code/day05-code/src/cn/itcast/day05/demo/HellowJava.java
+++ basic-code/day05-code/src/cn/itcast/day05/demo/HellowJava.java
@@ -4,6 +4,7 @@ public class HellowJava {
     public static void main(String[] args) {
         for(int i =1 ; i <=20; i++){
             System.out.print("*");
+            System.out.print("*444444");
         }
     }
 }
